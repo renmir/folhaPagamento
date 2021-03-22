af051038e67782c1824d6ccf830a543e3f97b101
renmir <80703106+renmir@users.noreply.github.com>
2021-03-22 19:07:08 -0300

    Update folhaPag.java

--- folhaPag.java
+++ folhaPag.java
@@ -1,14 +1,27 @@
+import javax.swing.*;
 public class folhaPag {
+    static String resp;
+    static String perguntar = "O funcionario é do tipo: \nA. Comissionado \nB. Horista \nC. Assalariado \n";
     public static void main(String[] args) {
-        Remuneracao funcionario = new Remuneracao();
-        if ((funcionario.resp.equals("A"))) {
-            funcionario.comissionado();         // inicia o metodo comissionado dentro de remuneracao
+        while (true) {          // loop até usuario escolher uma das opções validas
+            resp = JOptionPane.showInputDialog(perguntar);
+            resp = resp.toUpperCase();
+            boolean respValida = resp.equals("A") || resp.equals("B") || resp.equals("C");
+            if (respValida) {
+                break;
+            } else { JOptionPane.showMessageDialog(null, "Resposta invalida. Por favor escolha A, B ou C."); }
         }
-        if ((funcionario.resp.equals("B"))) {
-            funcionario.horista();
+        if ((resp.equals("A"))) {
+            Remuneracao funcionario = new comissionado();
+            funcionario.pagamento();
         }
-        if ((funcionario.resp.equals("C"))) {
-            funcionario.assalariado();
+        if ((resp.equals("B"))) {
+            Remuneracao funcionario = new horista();
+            funcionario.pagamento();
+        }
+        if ((resp.equals("C"))) {
+            Remuneracao funcionario = new assalariado();
+            funcionario.pagamento();
         }
     }
 }
